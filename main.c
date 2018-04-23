9db6ded47f529bccaab2616af155302be3d31d1e
ioryogi <mchou@hotmail.fr>
2018-04-23 19:19:23 +0200

    First Commit

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "processus.h"
+
+int main() {
+    return EXIT_SUCCESS;
+}
---
+++ processus.h
@@ -0,0 +1,9 @@
+#ifndef PROJET_INFO_SE_PROCESSUS_H
+#define PROJET_INFO_SE_PROCESSUS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#endif
